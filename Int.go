564e68d9ee743e2f04d52c63af51c2e5240c33e9
Michael L. <mike@michaellawrenceit.com>
2026-04-01 16:27:34 -0500

    Update Int.go

--- Int.go
+++ Int.go
@@ -1,19 +1,13 @@
 package main
 
-import "fmt"
+import "errors"
 
-// Fetcher is the K.I.S.S. interface for all eclass helpers
-type Fetcher func(pv, pn string, toml map[string]string) string
+// Registry for eclass-specific URI builders
+var EclassRegistry = make(map[string]func(pv, pn string) string)
 
-var Registry = map[string]Fetcher{}
-
-func Register(name string, f Fetcher) {
-    Registry[name] = f
-}
-
-func GetURL(eclass, pv, pn string, toml map[string]string) (string, error) {
-    if f, ok := Registry[eclass]; ok {
-        return f(pv, pn, toml), nil
-    }
-    return "", fmt.Errorf("no helper for eclass: %s", eclass)
+func ResolveURL(eclass, pv, pn string) (string, error) {
+	if fn, ok := EclassRegistry[eclass]; ok {
+		return fn(pv, pn), nil
+	}
+	return "", errors.New("eclass not supported in registry")
 }
